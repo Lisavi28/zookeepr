e3123240ce4473f21c326156414cae938e3888cc
lisavi <lisavi_28@yahoo.com.mx>
2022-04-20 17:11:49 -0500

    fech front-end

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -95,6 +98,23 @@ app.post('/api/animals', (req, res) => {
     res.json(animal);
   }
 });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 
 
 app.listen(PORT, () => {
